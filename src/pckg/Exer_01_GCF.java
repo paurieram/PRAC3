d8609cdb1f0ef7404702cacad856808a5765b7bd
Pauri <78435604+paurieram@users.noreply.github.com>
2025-05-12 10:07:57 +0200

    1st ex

--- src/pckg/Exer_01_GCF.java
+++ src/pckg/Exer_01_GCF.java
@@ -9,13 +9,30 @@ public class Exer_01_GCF {
 		/* COMPLETE */
 		/* Write here a program that generates 100000 pairs of numbers
 		between [1, 5000] to check that the function calculates the GCF.*/
-
+		Random r= new Random();
+		for (int i = 0; i <= 100000; i++) {
+			int r1 = r.nextInt(4999)+1;
+			int r2 = r.nextInt(4999)+1;
+			if (iterativeGCF(r1, r2) == GCF(r1,r2)){
+				System.out.println("Correct");
+			}else{
+				System.out.println("Wrong");
+			}
+		}
 	}
 	
 	
 	/* COMPLETE */
 	// write here a recursive function that calculates the GCF using Dijkstra's algorithm
-
+	public static int GCF(int n1,int n2){
+		if(n1==n2){
+			return n1;
+		} else if (a > b) {
+            return mcd(a - b, b);
+        } else {
+            return mcd(a, b - a);
+        }
+	}
 
 	/*DO NOT MODIFY CODE BELOW*/
 	public static int iterativeGCF(int x, int y) {
